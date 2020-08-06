371b4fc610819d3a55598e6bd45f3b8e56045dde
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-06 21:20:55 +0600

    small change in threedots.js

--- threedots.js
+++ threedots.js
@@ -5,7 +5,7 @@ const array3 = [9,4,6,2,1];
 const finalArray = array1. concat(array2).concat([1223]).concat(array3);
 console.log(finalArray);
 
-const finalArray2 = [...array1, ...array2, ...[125], ...array3];
+const finalArray2 = [...array1, ...array2, 125, ...array3];
 console.log(finalArray2);
 
 const newArray = [1,2,3,4,5,6,7,8,9];
