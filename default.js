afc1c5c26991416f9992cb56e0d0ad9f54630752
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-06 10:04:54 +0600

    default value of parameter pass

---
+++ default.js
@@ -0,0 +1,6 @@
+function add(num1,num2 = 5){ // jodi kokhono num2 er value pass na kora tahole default value 5 niye kaj korbe. ar default value debar por o jodi value pass kora hoy tahole value pass er value niye kaj korbe.
+    return num1 + num2;
+}
+
+const total = add(10,30);
+console.log(total);
