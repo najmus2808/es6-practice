fad117033f1235eead42fcac4f68b550caa8e9bc
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-06 10:19:24 +0600

    template helfull for string concate

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Najmus";
+const lastName = "Sakib";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const email = `${firstName} ${lastName} ${20+30} just example`;
+console.log(email);
+
+const message = `I love you
+i miss you 
+But i don't need you`;
+console.log(message);
